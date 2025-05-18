2d18d2d92ddca865c9d750e8069b92720dc1b3de
Alievi1111 <alieviali1111@gmail.com>
2025-05-18 22:48:44 +0400

    feature/js

---
+++ script.js
@@ -0,0 +1,28 @@
+// Myname:Ali
+// Date:20.06.2003
+
+const firstName = "Ali";
+const lastName = "Aliev";
+let emailAdress = "alieviai1111@gmail.com";
+let phoneNumber = "555-48-08-33";
+let age = 21;
+let hobby = "eat and sleep";
+const isStudent = true;
+
+console.log("First Name:" + firstName);
+console.log("Last Name:" + lastName);
+console.log("Email Adress:" + emailAdress);
+console.log("Phone Number:" + phoneNumber);
+console.log("age:" + age);
+console.log("Hobby:" + hobby);
+console.log("Is Student:" + isStudent);
+
+hobby = "Learn new skills";
+age = 21 + 1;
+
+console.log("Changed Hobby:" + hobby);
+console.log("Changed age:" + 22);
+
+//This code contains personal information
+//Comments help us to understand the code easily
+//Boolean value "isstudent" help us to understand this is true or false
